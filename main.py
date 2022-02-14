e8c27c1830ed2aa9f1c299361b4487c76fdc0fcd
Filip Blajiu <filip.blajiu@frisbo.ro>
2022-02-14 22:42:26 +0200

    request basics

--- main.py
+++ main.py
@@ -41,6 +41,12 @@ def get_posts():
     return {"data": my_posts}
 
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
@@ -67,3 +73,25 @@ def get_post(id: int, response: Response):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"message": f"Post with id {id} not found!"}
     return {"data": post}
+
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist!")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} does not exist!")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
